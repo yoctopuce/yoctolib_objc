62f078ea39e1fee3174267403aa004b3091e8cf5
Yoctopuce <dev@yoctopuce.com>
2019-06-21 12:14:00 +0200

    "New build based on revison 35871"

--- Sources/yocto_api.h
+++ Sources/yocto_api.h
@@ -62,7 +62,7 @@
 
 extern NSMutableDictionary* YAPI_YFunctions;
 
-#define YOCTO_API_REVISION          "35838"
+#define YOCTO_API_REVISION          "35871"
 
 // yInitAPI argument
 #define Y_DETECT_NONE           0
