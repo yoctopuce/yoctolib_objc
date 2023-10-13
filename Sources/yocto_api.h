7142d891a7cad971a48768893c40048a6a3e9c44
Yoctopuce <dev@yoctopuce.com>
2023-10-13 08:57:35 +0200

    "New build based on revision 57097"

--- Sources/yocto_api.h
+++ Sources/yocto_api.h
@@ -62,7 +62,7 @@
 
 //extern NSMutableDictionary* YAPI_YFunctions;
 
-#define YOCTO_API_REVISION          "56784"
+#define YOCTO_API_REVISION          "57097"
 
 // yInitAPI argument
 #define Y_DETECT_NONE           0
